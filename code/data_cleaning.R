5e4066cafafe5143cdd6e2f931bc5b793f5c2ddc
Di Cecco <gdicecco@ad.unc.edu>
2020-03-03 15:25:21 -0500

    model occupancy trends

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -38,6 +38,7 @@ species_list <- species %>%
          sporder != "Cathartiformes")
 # write.csv(species_list, "data/species_list.csv", row.names = F)
 
+# Filter BBS to rpid = 101, runtype = 1, land birds != birds of prey
 counts.subs <- counts %>%
   filter(rpid == 101) %>%
   filter(aou %in% species_list$aou) %>%
@@ -67,19 +68,42 @@ bbs_map <- tm_shape(na) + tm_polygons() + tm_shape(route_sf) + tm_dots()
 # tmap_save(bbs_map, "figures/bbs_route_map_1970-2016.pdf")
 
 ### Route density 1990-2016 - 1-5 years in every five year time window
-
+# countrynum 124 = Canada, 840 = US
 counts_landcover_years <- counts.subs %>%
   mutate(y1 = case_when(countrynum == 124 ~ 1990,
                         countrynum == 840 ~ 1992),
          y2 = case_when(countrynum == 124 ~ 2010,
                         countrynum == 840 ~ 2016),
-         max_bins = case_when(countrynum == 124 ~ 5,
-                            countrynum == 840 ~ 6))
+         max_bins = case_when(countrynum == 124 ~ 4,
+                            countrynum == 840 ~ 5))
 
+## Count number of routes that meet a threshhold (surveys_per_window) of surveys per five year time window across study period
 counts_per_window <- function(surveys_per_window) {
   cont_routes <- counts_landcover_years %>%
     filter(year >= y1, year <= y2) %>%
-    mutate(year_bin = 5*floor(year/5)) %>%
+    group_by(countrynum) %>%
+    nest() %>%
+    mutate(year_bins = map2(countrynum, data, ~{
+      country <- .x
+      df <- .y
+      
+      if(country == 124) {
+        df %>%
+          mutate(year_bin = case_when(year >= 1990 & year <= 1994 ~ 1990,
+                                      year >= 1995 & year <= 1999 ~ 1995,
+                                      year >= 2000 & year <= 2004 ~ 2000,
+                                      TRUE ~ 2005))
+      } else {
+        df %>%
+          mutate(year_bin = case_when(year >= 1992 & year <= 1996 ~ 1992,
+                                      year >= 1997 & year <= 2001 ~ 1997,
+                                      year >= 2002 & year <= 2006 ~ 2002,
+                                      year >= 2007 & year <= 2011 ~ 2007,
+                                      year >= 2012 & year <= 2016 ~ 2012))
+      }
+    })) %>%
+    select(-data) %>%
+    unnest(cols = c(year_bins)) %>%
     group_by(max_bins, stateroute, year_bin) %>%
     summarize(n_years = n_distinct(year)) %>%
     filter(n_years >= surveys_per_window) %>%
@@ -97,9 +121,10 @@ bbs_density <- data.frame(years_per_window = 1:5) %>%
     }))
 
 ggplot(bbs_density, aes(x = years_per_window, y = routes)) + geom_point() + geom_line(cex = 1) + theme_classic(base_size = 15)
+ggsave("figures/bbs_route_density.pdf")
 
 pdf("figures/bbs_route_map_1990-2016_surveys_per_window.pdf")
-for(i in c(1:2)) {
+for(i in c(1:5)) {
   output <- counts_per_window(i)
   cont_routes <- output$df
   routes_short <- route_sf %>%
@@ -110,7 +135,7 @@ for(i in c(1:2)) {
 }
 dev.off()
 
-counts_subs <- counts_per_window(1)
+counts_subs <- counts_per_window(3)
 routes_subs <- counts_subs$df
 
 # write.csv(routes_subs, "data/bbs_route_subset_1990-2016.csv", row.names = F)
--- code/occupancy_analysis.R
+++ code/occupancy_analysis.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(purrr)
+library(broom)
 
 ## Plotting theme
 
@@ -10,7 +11,7 @@ theme_set(theme_classic(base_size = 15))
 ## Read in data
 
 log_abund <- read.csv("data/bbs_subset_1970-2016_logabund.csv", stringsAsFactors = F)
-routes_subs <- read.csv("data/bbs_route_subset_1990-2016.csv", stringsAsFactors = F)
+routes_subs <- read.csv("data/bbs_route_subset_1990-2016.csv", stringsAsFactors = F) # only routes with at least 3 surveys per 5 year window
 species_list <- read.csv("data/species_list.csv", stringsAsFactors = F)
 
 log_abund_subs <- log_abund %>%
@@ -52,19 +53,20 @@ spp_colext <- spp_occ_deltas %>%
   nest() %>%
   mutate(rte_col = map_dbl(data, ~{
     df <- .
-    n_distinct(df$stateroute[df$d1 == 0 & df$d2 > 0])/n_distinct(df$stateroute)
+    n_distinct(df$stateroute[df$d1 == 0 & df$d2 > 0])/n_distinct(df$stateroute[df$d2 > 0])
   }),
   rte_ext = map_dbl(data, ~{
     df <- .
-    n_distinct(df$stateroute[df$d1 > 0 & df$d2 == 0])/n_distinct(df$stateroute)
+    n_distinct(df$stateroute[df$d1 > 0 & df$d2 == 0])/n_distinct(df$stateroute[df$d1 > 0])
   }),
   rte_dTrans = map_dbl(data, ~{
     df <- .
-    trans_d1 <- n_distinct(df$stateroute[df$d1 < 0.3 & df$d1 > 0])/n_distinct(df$stateroute)
-    trans_d2 <- n_distinct(df$stateroute[df$d2 < 0.3 & df$d2 > 0])/n_distinct(df$stateroute)
+    trans_d1 <- n_distinct(df$stateroute[df$d1 <= 0.3 & df$d1 > 0])/n_distinct(df$stateroute[df$d1 > 0])
+    trans_d2 <- n_distinct(df$stateroute[df$d2 <= 0.3 & df$d2 > 0])/n_distinct(df$stateroute[df$d2 > 0])
     trans_d2-trans_d1
   }),
-  n_routes = map_dbl(data, nrow)) %>%
+  n_routes = map_dbl(data, nrow),
+  colext = rte_col/rte_ext) %>%
   filter(n_routes > 20)
 
 ggplot(spp_colext, aes(x = rte_col)) + geom_histogram(bins = 20, col = "white") +
@@ -81,3 +83,49 @@ ggplot(spp_colext, aes(x = rte_dTrans)) + geom_histogram(bins = 20, col = "white
   geom_vline(aes(xintercept = mean(rte_dTrans)), col = "red", cex = 1) +
   labs(y = "Count", x = "Change in percent routes transient")
 ggsave("figures/hist_dTransient.pdf")
+
+ggplot(spp_colext, aes(x = colext)) + geom_histogram(bins = 20, col = "white") + 
+  geom_vline(aes(xintercept = mean(colext)), col = "red", cex = 1) +
+  labs(x = "Colonization rate/extinction rate", y = "Count")
+ggsave("figures/hist_ratio_col_ext.pdf")
+
+## Species occupancy by year bin
+
+spp_occ_year_bins <- log_abund_subs %>%
+  group_by(countrynum) %>%
+  nest() %>%
+  mutate(year_bins = map2(countrynum, data, ~{
+    country <- .x
+    df <- .y
+    
+    if(country == 124) {
+      df %>%
+        mutate(year_bin = case_when(year >= 1990 & year <= 1994 ~ 1990,
+                                    year >= 1995 & year <= 1999 ~ 1995,
+                                    year >= 2000 & year <= 2004 ~ 2000,
+                                    TRUE ~ 2005))
+    } else {
+      df %>%
+        mutate(year_bin = case_when(year >= 1992 & year <= 1996 ~ 1992,
+                                    year >= 1997 & year <= 2001 ~ 1997,
+                                    year >= 2002 & year <= 2006 ~ 2002,
+                                    year >= 2007 & year <= 2011 ~ 2007,
+                                    year >= 2012 & year <= 2016 ~ 2012))
+    }
+  })) %>%
+  select(-data) %>%
+  unnest(cols = c(year_bins)) %>%
+  group_by(aou, english_common_name, stateroute, year_bin) %>%
+  summarize(years_obs = n_distinct(year)) %>%
+  left_join(routes_subs, by = c("stateroute", "year_bin")) %>%
+  mutate(occ = years_obs/n_years) %>%
+  group_by(max_bins, aou, english_common_name, stateroute) %>%
+  nest() %>%
+  mutate(n_points = map_dbl(data, ~nrow(.))) %>%
+  filter(n_points == max_bins) %>%
+  mutate(occ_mod = map(data, ~glm(occ~year_bin, data = ., family = "binomial", weights = n_years)),
+         tidy_mod = map(occ_mod, ~tidy(.))) %>%
+  select(-data, -occ_mod) %>%
+  unnest(cols = c("tidy_mod"))
+
+# write.csv(spp_occ_year_bins, "data/occupancy_trend_mods.csv", row.names = F)

2867a11c87a30c65d94e02623498b360cc7d777a
gdicecco <gdicecco@live.unc.edu>
2020-04-07 10:02:30 -0400

    EDA of directionality values with all vs excl transient species

--- code/data_cleaning.R
+++ code/data_cleaning.R
@@ -14,10 +14,15 @@ na <- world %>%
   filter(continent == "North America")
 
 ## BBS 2017 Version
-routes <- read.csv("\\\\BioArk\\HurlbertLab\\Databases\\BBS\\2017\\bbs_routes_20170712.csv")
-counts <- read.csv("\\\\BioArk\\hurlbertlab\\Databases\\BBS\\2017\\bbs_counts_20170712.csv")
-species <- read.csv("\\\\BioArk\\hurlbertlab\\Databases\\BBS\\2017\\bbs_species_20170712.csv")
-weather <- read.csv("\\\\BioArk\\hurlbertlab\\Databases\\BBS\\2017\\bbs_weather_20170712.csv")
+
+# Append correct BioArk path
+info <- sessionInfo()
+bioark <- ifelse(grepl("apple", info$platform), "/Volumes", "\\\\BioArk")
+
+routes <- read.csv(paste0(bioark, "/HurlbertLab/Databases/BBS/2017/bbs_routes_20170712.csv"))
+counts <- read.csv(paste0(bioark, "/HurlbertLab/Databases/BBS/2017/bbs_counts_20170712.csv"))
+species <- read.csv(paste0(bioark, "/HurlbertLab/Databases/BBS/2017/bbs_species_20170712.csv"))
+weather <- read.csv(paste0(bioark, "/HurlbertLab/Databases/BBS/2017/bbs_weather_20170712.csv"))
 
 routes$stateroute <- routes$statenum*1000 + routes$route
 weather$stateroute <-weather$statenum*1000 + weather$route
@@ -67,15 +72,15 @@ route_sf <- cont_routes %>%
 bbs_map <- tm_shape(na) + tm_polygons() + tm_shape(route_sf) + tm_dots()
 # tmap_save(bbs_map, "figures/bbs_route_map_1970-2016.pdf")
 
-### Route density 1990-2016 - 1-5 years in every five year time window
+### Route density 1990-2016 - 1-4 years in every four year time window
 # countrynum 124 = Canada, 840 = US
 counts_landcover_years <- counts.subs %>%
   mutate(y1 = case_when(countrynum == 124 ~ 1990,
                         countrynum == 840 ~ 1992),
          y2 = case_when(countrynum == 124 ~ 2010,
                         countrynum == 840 ~ 2016),
-         max_bins = case_when(countrynum == 124 ~ 4,
-                            countrynum == 840 ~ 5))
+         max_bins = case_when(countrynum == 124 ~ 5,
+                            countrynum == 840 ~ 6))
 
 ## Count number of routes that meet a threshhold (surveys_per_window) of surveys per five year time window across study period
 counts_per_window <- function(surveys_per_window) {
@@ -89,16 +94,18 @@ counts_per_window <- function(surveys_per_window) {
       
       if(country == 124) {
         df %>%
-          mutate(year_bin = case_when(year >= 1990 & year <= 1994 ~ 1990,
-                                      year >= 1995 & year <= 1999 ~ 1995,
-                                      year >= 2000 & year <= 2004 ~ 2000,
-                                      TRUE ~ 2005))
+          mutate(year_bin = case_when(year >= 1990 & year <= 1993 ~ 1990,
+                                      year >= 1994 & year <= 1997 ~ 1994,
+                                      year >= 1998 & year <= 2001 ~ 1998,
+                                      year >= 2002 & year <= 2005 ~ 2002,
+                                      TRUE ~ 2006))
       } else {
         df %>%
-          mutate(year_bin = case_when(year >= 1992 & year <= 1996 ~ 1992,
-                                      year >= 1997 & year <= 2001 ~ 1997,
-                                      year >= 2002 & year <= 2006 ~ 2002,
-                                      year >= 2007 & year <= 2011 ~ 2007,
+          mutate(year_bin = case_when(year >= 1992 & year <= 1995 ~ 1992,
+                                      year >= 1996 & year <= 1999 ~ 1996,
+                                      year >= 2000 & year <= 2003 ~ 2000,
+                                      year >= 2004 & year <= 2007 ~ 2004,
+                                      year >= 2008 & year <= 2011 ~ 2008,
                                       year >= 2012 & year <= 2016 ~ 2012))
       }
     })) %>%
@@ -114,7 +121,7 @@ counts_per_window <- function(surveys_per_window) {
   return(list(df = cont_routes, n_routes = length(unique(cont_routes$stateroute))))
 }
 
-bbs_density <- data.frame(years_per_window = 1:5) %>%
+bbs_density <- data.frame(years_per_window = 1:4) %>%
   mutate(routes = map_dbl(years_per_window, ~{
       output <- counts_per_window(.)
       output$n_routes
@@ -124,7 +131,7 @@ ggplot(bbs_density, aes(x = years_per_window, y = routes)) + geom_point() + geom
 ggsave("figures/bbs_route_density.pdf")
 
 pdf("figures/bbs_route_map_1990-2016_surveys_per_window.pdf")
-for(i in c(1:5)) {
+for(i in c(1:4)) {
   output <- counts_per_window(i)
   cont_routes <- output$df
   routes_short <- route_sf %>%
@@ -139,6 +146,31 @@ counts_subs <- counts_per_window(3)
 routes_subs <- counts_subs$df
 
 # write.csv(routes_subs, "data/bbs_route_subset_1990-2016.csv", row.names = F)
+
+bbs_subset <- counts.subs %>%
+  filter(stateroute %in% routes_subs$stateroute) %>%
+  filter(year >= 1990)
+
+# write.csv(bbs_subset, "data/bbs_counts_subset_1990-2016.csv", row.names = F)
+
+## Sample size per BCR
+
+bcr_n <- bbs_subset %>%
+  group_by(bcr) %>%
+  summarize(n_routes = n_distinct(stateroute))
+
+## Figure: map of BCRs with BBS routes, number of routes per BCR that meet sampling density thresholds
+
+bcrs <- read_sf(paste0(bioark, "/HurlbertLab/DiCecco/bcr_terrestrial_shape/BCR_terrestrial_master.shp")) %>%
+  filter(COUNTRY == "USA" | COUNTRY == "CANADA") %>%
+  st_crop(c(xmin = -178, ymin = 18.9, xmax = -53, ymax = 60)) %>%
+  filter(PROVINCE_S != "ALASKA" & PROVINCE_S != "HAWAIIAN ISLANDS" & PROVINCE_S != "NUNAVUT" & PROVINCE_S != "NORTHWEST TERRITORIES" & PROVINCE_S != "YUKON") %>%
+  filter(WATER == 3) %>%
+  left_join(bcr_n, by = c("BCR" = "bcr"))
+
+bcr_route_density <- tm_shape(bcrs) + tm_polygons(col = "n_routes", palette = "YlGnBu", title = "BBS routes") +
+  tm_layout(scale = 2)
+tmap_save(bcr_route_density, "figures/bcr_route_density.pdf")
 
 ## Log abundance
 
--- code/trajectory_analysis.R
+++ code/trajectory_analysis.R
@@ -15,7 +15,11 @@ theme_set(theme_classic(base_size = 15))
 
 ## Read in data
 
+# BBS sampled every 5 years from 1970 to 2016
 log_abund <- read.csv("data/bbs_subset_1970-2016_logabund.csv", stringsAsFactors = F)
+
+# BBS sampled 3/4 years every 4 year time window
+bbs_subset <- read.csv("data/bbs_counts_subset_1990-2016.csv", stringsAsFactors = F)
 
 routes <- read.csv("/Volumes/hurlbertlab/databases/BBS/2017/bbs_routes_20170712.csv", stringsAsFactors = F) %>%
   mutate(stateroute = statenum*1000 + route)
@@ -112,27 +116,22 @@ ggplot(logabund_wide, aes(x = dir50, y = dir25)) + geom_point() +
 ggsave("figures/directionality_time_series_comparison.pdf")
 
 ## Model of directionality ~ land cover change + climate change
-
-dir_model <- logabund_wide %>%
-  select(-data, -dir50) %>%
-  left_join(bbs_climate) %>%
-  left_join(bbs_landcover) %>%
-  filter(env == "tmin") %>%
-  na.omit() ## Need to ID why there are NAs in this - all routes should be present in climate/land cover
-# 629 routes
-
-# GLM model -- Talk to Allen/James - this needs to be spatial CAR + GLM (directionality is bounded 0-1)
-## Possibly CARBayes?
+# 935 routes
 
 # Coordinates of BBS routes
 
-coords_bbs <- dir_model %>%
+coords_bbs <- bbs_subset %>%
   ungroup() %>%
   dplyr::select(stateroute) %>%
   distinct() %>%
   left_join(routes) %>%
   dplyr::select(stateroute, bcr, longitude, latitude) %>%
-  st_as_sf(coords = c("longitude", "latitude"))
+  st_as_sf(coords = c("longitude", "latitude")) %>%
+  st_set_crs(4236)
+
+bcr_sample_size <- coords_bbs %>%
+  group_by(bcr) %>%
+  summarize(n_routes = n_distinct(stateroute))
 
 ## Find nearest grouped BBS routes within BCR
 
@@ -153,27 +152,180 @@ dist_bcr <- function(strte) {
   return(dist_df)
 }
 
-bbs_route_distances <- map_dfr(unique(dir_model$stateroute), ~dist_bcr(.))
+# units in m
+bbs_route_distances <- map_dfr(unique(bbs_subset$stateroute), ~dist_bcr(.))
+
+stateroutes <- data.frame(stateroute = unique(bbs_subset$stateroute))
+  
+bbs_distances_km <- bbs_route_distances %>%
+  mutate(dist_km = distance/1000)
+
+mean_bbs_distances <- stateroutes %>%
+  mutate(scale = map(stateroute, ~data.frame(scale = rep(1:25)))) %>%
+  unnest(cols = c(scale)) %>%
+  mutate(model_input = map2(stateroute, scale, ~{
+    bbs_distances_km %>%
+      filter(focal_rte == .x) %>%
+      arrange(dist_km) %>%
+      slice(1:.y)
+  }),
+  mean_dist = map_dbl(model_input, ~mean(.$dist_km)))
+  
+mean_distances <- mean_bbs_distances %>%
+  group_by(scale) %>%
+  summarize(mean_dist_rtes = mean(mean_dist))
+
+mean_bcr_distances <- mean_bbs_distances %>%
+  left_join(coords_bbs) %>%
+  group_by(bcr, scale) %>%
+  summarize(mean_dist_bcr = mean(mean_dist))
+
+bcr_subset <- bcr_sample_size %>%
+  filter(n_routes > 25)
+
+ggplot(filter(mean_bcr_distances, bcr %in% bcr_subset$bcr), 
+       aes(x = scale, y = mean_dist_bcr, col = bcr, group = bcr)) + 
+  geom_line() +
+  scale_color_viridis_c() +
+  labs(x = "Scale (routes)", y = "Mean distance between routes (km)", col = "BCR")
+ggsave("figures/scale_aggregated_routes.pdf")
 
 ## Model for 1/2 routes
 
-## Model from 1 route up to nearest 30 routes
+#### Need half route climate, land cover, BBS data at stop level!!
 
-log_abund_wider <- log_abund %>%
+## Model from 1 route up to nearest 25 routes
+## One data point per 4 year time window
+## ID core and transient species
+
+# All species
+log_abund_wider <- bbs_subset %>%
+  filter(bcr %in% bcr_subset$bcr) %>%
+  mutate(y1 = case_when(countrynum == 124 ~ 1990,
+                        countrynum == 840 ~ 1992),
+         y2 = case_when(countrynum == 124 ~ 2010,
+                        countrynum == 840 ~ 2016),
+         max_bins = case_when(countrynum == 124 ~ 5,
+                              countrynum == 840 ~ 6)) %>%
+  filter(year >= y1, year <= y2) %>%
+  group_by(countrynum) %>%
+  nest() %>%
+  mutate(year_bins = map2(countrynum, data, ~{
+    country <- .x
+    df <- .y
+    
+    if(country == 124) {
+      df %>%
+        mutate(year_bin = case_when(year >= 1990 & year <= 1993 ~ 1990,
+                                    year >= 1994 & year <= 1997 ~ 1994,
+                                    year >= 1998 & year <= 2001 ~ 1998,
+                                    year >= 2002 & year <= 2005 ~ 2002,
+                                    TRUE ~ 2006))
+    } else {
+      df %>%
+        mutate(year_bin = case_when(year >= 1992 & year <= 1995 ~ 1992,
+                                    year >= 1996 & year <= 1999 ~ 1996,
+                                    year >= 2000 & year <= 2003 ~ 2000,
+                                    year >= 2004 & year <= 2007 ~ 2004,
+                                    year >= 2008 & year <= 2011 ~ 2008,
+                                    year >= 2012 & year <= 2016 ~ 2012))
+    }
+  })) %>%
+  select(-data) %>%
+  unnest(cols = c(year_bins)) %>%
+  group_by(stateroute, aou, year_bin) %>%
+  summarize(mean_abund = mean(speciestotal),
+            log_abund = log10(mean_abund)) %>%
+  dplyr::select(stateroute, aou, year_bin, log_abund) %>%
   pivot_wider(names_from = aou, values_from = log_abund, values_fn = list(log_abund = mean))
 
-stateroutes <- data.frame(stateroute = unique(dir_model$stateroute))
+# Excluding transient species
+log_abund_core <- bbs_subset %>%
+  filter(bcr %in% bcr_subset$bcr) %>%
+  mutate(y1 = case_when(countrynum == 124 ~ 1990,
+                        countrynum == 840 ~ 1992),
+         y2 = case_when(countrynum == 124 ~ 2010,
+                        countrynum == 840 ~ 2016),
+         max_bins = case_when(countrynum == 124 ~ 5,
+                              countrynum == 840 ~ 6)) %>%
+  filter(year >= y1, year <= y2) %>%
+  group_by(countrynum) %>%
+  nest() %>%
+  mutate(year_bins = map2(countrynum, data, ~{
+    country <- .x
+    df <- .y
+    
+    if(country == 124) {
+      df %>%
+        mutate(year_bin = case_when(year >= 1990 & year <= 1993 ~ 1990,
+                                    year >= 1994 & year <= 1997 ~ 1994,
+                                    year >= 1998 & year <= 2001 ~ 1998,
+                                    year >= 2002 & year <= 2005 ~ 2002,
+                                    TRUE ~ 2006))
+    } else {
+      df %>%
+        mutate(year_bin = case_when(year >= 1992 & year <= 1995 ~ 1992,
+                                    year >= 1996 & year <= 1999 ~ 1996,
+                                    year >= 2000 & year <= 2003 ~ 2000,
+                                    year >= 2004 & year <= 2007 ~ 2004,
+                                    year >= 2008 & year <= 2011 ~ 2008,
+                                    year >= 2012 & year <= 2016 ~ 2012))
+    }
+  })) %>%
+  select(-data) %>%
+  unnest(cols = c(year_bins)) %>%
+  group_by(stateroute, aou, year_bin) %>%
+  summarize(n_years = n_distinct(year),
+            mean_abund = mean(speciestotal),
+            log_abund = log10(mean_abund)) %>%
+  filter(n_years > 1) %>%
+  dplyr::select(stateroute, aou, year_bin, log_abund) %>%
+  pivot_wider(names_from = aou, values_from = log_abund, values_fn = list(log_abund = mean))
 
-## At each scale (1 route, up to nearest 30 routes within BCR) 
+# Directionality at 1 route scale, core vs. transients
+
+dir_all_spp <- log_abund_wider %>%
+  group_by(stateroute) %>%
+  nest() %>%
+  mutate(dir_all = map_dbl(data, ~{
+    df <- .
+    dist <- dist(df[, -1])
+    trajectoryDirectionality(dist, sites = rep(1, nrow(df)), surveys = df$year_bin)
+  }))
+
+dir_core_spp <- log_abund_core %>%
+  group_by(stateroute) %>%
+  nest() %>%
+  mutate(dir_core = map_dbl(data, ~{
+    df <- .
+    dist <- dist(df[, -1])
+    trajectoryDirectionality(dist, sites = rep(1, nrow(df)), surveys = df$year_bin)
+  }))
+
+dir_compare <- dir_all_spp %>%
+  dplyr::select(-data) %>%
+  left_join(dir_core_spp) %>%
+  dplyr::select(-data)
+
+ggplot(dir_compare, aes(x = dir_all, y = dir_core)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1) + labs(x = "Directionality (all spp)", y = "Directionality (excl. transients)")
+ggsave("figures/directionality_all_vs_core.pdf")
+
+## At each scale (1 route, up to nearest 25 routes within BCR) 
 ## Get max land cover delta from raw land cover data and get trend in Tmin and trend in Tmax
 ## Calculate directionality for each grouping of routes: average log(abundance) across routes when pooled
-## Fit models across BBS routes for each scale (1:30)
+## Fit models across BBS routes for each scale (1:25)
 ## Variance partitioning of land cover and climate variables explaining trajectory 
 ## Directionality ~ tmin + tmax + max(deltaLandCover)
 ## Variance partitioning with ecospat.varpat(model.1, model.2, model.12) in ecospat
 
-scale_model_input <- stateroutes %>%
-  mutate(scale = map(stateroute, ~data.frame(scale = rep(1:30)))) %>%
+scale_model_input <- bbs_subset %>%
+  ungroup() %>%
+  distinct(stateroute, bcr) %>%
+  filter(bcr %in% bcr_subset$bcr) %>%
+  dplyr::select(stateroute) %>%
+  mutate(scale = map(stateroute, ~data.frame(scale = rep(1:25)))) %>%
   unnest(cols = c(scale)) %>%
   mutate(model_input = map2(stateroute, scale, ~{
     bbs_route_distances %>%
@@ -184,26 +336,32 @@ scale_model_input <- stateroutes %>%
   input_vars = map(model_input, ~{
     df <- .
     
-    land_cover <- landcover_na %>%
-      filter(stateroute %in% df$stateroute)
-      
-    max_delta <- possibly_max_delta(land_cover)
-    
-    climate <- bbs_climate_avgs %>%
-      filter(stateroute %in% df$stateroute)
-    
-    trend_tmax <- coef(lm(mean_tmax ~ year, data = climate))[[2]]
-    trend_tmin <- coef(lm(mean_tmin ~ year, data = climate))[[2]]
+    # land_cover <- landcover_na %>%
+    #   filter(stateroute %in% df$stateroute)
+    #   
+    # max_delta <- possibly_max_delta(land_cover)
+    # 
+    # climate <- bbs_climate_avgs %>%
+    #   filter(stateroute %in% df$stateroute)
+    # 
+    # trend_tmax <- coef(lm(mean_tmax ~ year, data = climate))[[2]]
+    # trend_tmin <- coef(lm(mean_tmin ~ year, data = climate))[[2]]
     
     log_abund <- log_abund_wider %>%
-      filter(stateroute %in% df$stateroute) %>%
-      dplyr::select(-countrynum) %>%
-      group_by(year) %>%
-      summarize_all(mean, na.rm = T)
-    abund_dist <- dist(log_abund)
-    dir25 <- trajectoryDirectionality(abund_dist, sites = rep(1, nrow(log_abund)), surveys = log_abund$year)
+      filter(stateroute %in% df$stateroute)
+    abund_dist <- dist(log_abund[, -1])
+    dir_all <- trajectoryDirectionality(abund_dist, sites = rep(1, nrow(log_abund)), surveys = log_abund$year_bin)
     
-    data.frame(focal_rte = unique(df$focal_rte), max_lc = max_delta, trend_tmax = trend_tmax, trend_tmin = trend_tmin, dir25 = dir25)
+    log_abund_core <- log_abund_core %>%
+      filter(stateroute %in% df$stateroute)
+    abund_dist_core <- dist(log_abund_core[, -1])
+    dir_core <- trajectoryDirectionality(abund_dist_core, sites = rep(1, nrow(log_abund_core)), surveys = log_abund_core$year_bin)
+    
+    # data.frame(focal_rte = unique(df$focal_rte), max_lc = max_delta, 
+    #            trend_tmax = trend_tmax, trend_tmin = trend_tmin, 
+    #            dir_all = dir_all, dir_core = dir_core)
+    
+    data.frame(focal_rte = unique(df$focal_rte), dir_all = dir_all, dir_core = dir_core)
   
   }))
 
@@ -213,9 +371,19 @@ scale_model_variables <- scale_model_input %>%
   group_by(scale) %>%
   nest()
 
-# scale_model_variables_unnest <- scale_model_variables %>%
-#   unnest(cols = c(data))
+scale_model_variables_unnest <- scale_model_variables %>%
+  unnest(cols = c(data))
 # write.csv(scale_model_variables_unnest, "data/scale_model_input.csv", row.names = F)
+
+ggplot(scale_model_variables_unnest, aes(x = scale, y = dir_all, col = focal_rte, group = focal_rte)) + 
+  geom_line(alpha = 0.1) + labs(x = "Scale (routes)", y = "Directionality (all spp.)", col = "Stateroute")
+ggsave("figures/directionality_scale.pdf")
+
+ggplot(filter(scale_model_variables_unnest, scale == 25), aes(x = dir_all, y = dir_core)) + 
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1) +
+  labs(x = "Directionality (all spp.)", y = "Directionality (excl. transients)", title = "25 routes")
+ggsave("figures/directionality_all_vs_core_25.pdf")
 
 scale_model_output <- data.frame(scale = c(), part1 = c(), part2 = c(), joined = c(), unexpl = c())
 for(i in 1:30) {

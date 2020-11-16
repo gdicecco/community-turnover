0bddfc7926d05f0dffca672914637f7d81db27ff
gdicecco <gdicecco@live.unc.edu>
2020-11-16 09:34:26 -0500

    null model results gain/loss

--- code/cm_null_model.R
+++ code/cm_null_model.R
@@ -25,14 +25,13 @@ cm_null_input <- bbs_subset %>%
       group_by(year_bin, aou) %>%
       summarize_all(mean, na.rm = T) %>%
       left_join(cwm_traits) %>%
-      group_by(year_bin) %>%
-      summarize(n_spp = n_distinct(aou))
+      group_by(year_bin)
     
   }))
 
 cm_null_input_unnest <- cm_null_input %>%
   dplyr::select(-model_input) %>%
-  unnest()
+  unnest() 
 
 spp_changes <- cm_null_input %>%
   dplyr::select(-model_input) %>%
@@ -40,18 +39,44 @@ spp_changes <- cm_null_input %>%
   group_by(stateroute, scale) %>%
   mutate(n_bins = n_distinct(year_bin)) %>%
   filter(n_bins == 6) %>%
-  summarize(start_spp = n_spp[year_bin == min(year_bin, na.rm = T)],
-            end_spp = n_spp[year_bin == max(year_bin, na.rm = T)],
-    delta_spp = n_spp[year_bin == max(year_bin, na.rm = T)] - n_spp[year_bin == min(year_bin, na.rm = T)])
+  nest() %>%
+  mutate(turnover = map(data, ~{
+    df <- .
+    
+    years <- unique(df$year_bin)
+    
+    start <- length(unique(df$aou[df$year_bin == min(df$year_bin, na.rm = T)]))
+    
+    transitions <- data.frame(transition = c(1:5), yr_start = c(rep(NA,5)),
+                              yr_end = c(rep(NA, 5)), 
+                              gain = c(rep(NA, 5)), loss = c(rep(NA, 5)))
+    
+    for(i in 1:5) {
+      y1 <- years[i]
+      y2 <- years[i + 1]
+      
+      list1 <- unique(df$aou[df$year_bin == y1])
+      list2 <- unique(df$aou[df$year_bin == y2])
+      
+      g <- length(list2[!(list2 %in% list1)])
+      l <- length(list1[!(list1 %in% list2)])
+      
+      transitions[i, 2] <- y1
+      transitions[i, 3] <- y2
+      transitions[i, 4] <- g
+      transitions[i, 5] <- l
+    }
+    
+    transitions$start_n <- start
+    
+    transitions
+    
+  }))
 
-local_change <- ggplot(filter(spp_changes, scale == 2), aes(x = delta_spp)) + geom_histogram() +
-  labs(x = "Net change in species", main = "Scale = 2 routes")
 
-regional_change <- ggplot(filter(spp_changes, scale == 21), aes(x = delta_spp)) + geom_histogram() +
-  labs(x = "Net change in species", main = "Scale = 21 routes")
-
-plot_grid(local_change, regional_change, nrow = 1)
-ggsave("figures/cm_change_in_spp.pdf", units = "in", width = 10, height = 5)
+cm_transitions <- spp_changes %>%
+  dplyr::select(-data) %>%
+  unnest(cols = c("turnover"))
 
 ### Species list for each BCR
 bcr_spp_list <- bbs_subset %>%
@@ -62,13 +87,19 @@ bcr_spp_list <- bbs_subset %>%
   distinct(aou) %>%
   left_join(cwm_traits)
 
-cm_null_means <- cm_null_input_unnest %>%
-  left_join(routes) %>%
-  mutate(null_cm = map2(bcr, n_spp, ~{
+cm_null_means <- cm_transitions %>%
+  left_join(dplyr::select(routes, stateroute, bcr)) %>%
+  group_by(stateroute, scale, bcr) %>%
+  nest() %>%
+  mutate(null_cm = map2(bcr, data, ~{
     b <- .x
-    n <- .y
+    df <- .y
     
-    res <- data.frame(sim = c(), cm_temp_mean = c(), 
+    start <- unique(df$start_n)
+    
+    start_y <- min(df$yr_start)
+    
+    res <- data.frame(sim = c(), year_bin = c(), cm_temp_mean = c(), 
                       cm_temp_range = c(),
                       cm_for_mean = c(),
                       cm_for_range = c(),
@@ -77,18 +108,60 @@ cm_null_means <- cm_null_input_unnest %>%
     for(i in 1:999) {
       samp <- bcr_spp_list %>%
         filter(bcr == b) %>%
-        sample_n(n)
+        sample_n(start)
       
-      cm <- data.frame(sim = i, cm_temp_mean = mean(samp$temp_mean),
-                       cm_temp_range = mean(samp$temp_range),
-                       cm_for_mean = mean(samp$propFor),
-                       cm_for_range = mean(samp$for_range),
-                       cm_bodysize = mean(samp$logMass))
+      res <- rbind(res, data.frame(sim = i, year_bin = start_y,
+                              cm_temp_mean = mean(samp$temp_mean, na.rm = T),
+                              cm_temp_range = mean(samp$temp_range, na.rm = T),
+                              cm_for_mean = mean(samp$propFor, na.rm = T),
+                              cm_for_range = mean(samp$for_range, na.rm = T),
+                              cm_bodysize = mean(samp$logMass, na.rm = T)))
       
-      res <- rbind(res, cm)
+      for(j in 1:5) {
+        g <- df$gain[j]
+        l <- df$loss[j]
+        
+        yr <- df$yr_end[j]
+        
+        g_spp <- bcr_spp_list %>%
+          filter(bcr == b) %>%
+          filter(!(aou %in% samp$aou)) %>%
+          sample_n(g)
+        
+        l_spp <- samp %>%
+          sample_n(l)
+        
+        samp <- samp %>%
+          bind_rows(g_spp) %>%
+          filter(!(aou %in% l_spp$aou))
+        
+        cm <- data.frame(sim = i, year_bin = yr,
+                         cm_temp_mean = mean(samp$temp_mean, na.rm = T),
+                         cm_temp_range = mean(samp$temp_range, na.rm = T),
+                         cm_for_mean = mean(samp$propFor, na.rm = T),
+                         cm_for_range = mean(samp$for_range, na.rm = T),
+                         cm_bodysize = mean(samp$logMass, na.rm = T))
+        
+        res <- rbind(res, cm)
+        
+      }
       
     }
     
     res
     
   }))
+
+local_null <- cm_null_means %>%
+  filter(scale == 2) %>%
+  dplyr::select(-data) %>%
+  unnest(cols = c("null_cm"))
+write.csv(local_null, "bigdata/community_mean_null_mod_res_local_scale.csv", row.names = F)
+
+regional_null <- cm_null_means %>%
+  filter(scale == 21) %>%
+  dplyr::select(-data) %>%
+  unnest(cols = c("null_cm"))
+write.csv(regional_null, "bigdata/community_mean_null_mod_res_regional_scale.csv", row.names = F)
+
+
